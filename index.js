ba05bd5634b087e96ffdcfe700294e2837e172cd
hqayumie <heelaqayumie@gmail.com>
2023-01-21 19:47:15 -0500

    added config.js and index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server for ${activity} running on port ${PORT}!`);
+  });
+});
