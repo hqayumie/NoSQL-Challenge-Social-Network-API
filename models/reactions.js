7d10c51a09ce7140a7d7eb62a92a3d20ecd24a6b
hqayumie <heelaqayumie@gmail.com>
2023-01-22 14:59:11 -0500

    added reactions.js and userControllers.js

---
+++ models/reactions.js
@@ -0,0 +1,31 @@
+// Importing Dependencies 
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+// Reaction is a subdocument of Thought
+const ReactionSchema = new Schema({ 
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        // default value is set to a new ObjectId
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    }, 
+    createdAt: {
+        type: Date, 
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+},
+{
+    toJSON: {
+        getters: true
+    }
+});
